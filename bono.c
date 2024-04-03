82a1080c86a9941fa29e6943ef5e147b92dfcb5e
soyudea <omara.torres@udea.edu.co>
2024-04-03 15:29:46 -0500

    Actaualizando codigo del bono

--- bono.c
+++ bono.c
@@ -3,3 +3,17 @@
 #include <stdlib.h>
 #include <unistd.h> // Para fork(), sleep()
 #include <sys/wait.h>
+void animacion() {
+    system("clear"); // Limpiar la pantalla
+    printf("\033[?25l"); // Ocultar el cursor
+    printf("Cargando");
+    fflush(stdout); // Forzar la salida del búfer
+
+    for (int i = 0; i < 10; ++i) {
+        printf(".");
+        fflush(stdout);
+        usleep(200000); // Esperar 200ms
+    }
+
+    printf("\n\033[?25h"); // Mostrar el cursor
+}
